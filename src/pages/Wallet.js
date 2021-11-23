36ab33a70e42db9d2125ef5d1abad24d66d46176
Vanessa <vanessamrios@gmail.com>
2021-11-23 11:04:29 +0100

    (Req 03) Exibe userEmail no header do componente Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <div data-testid="email-field">{ userEmail }</div>
+          <div data-testid="total-field">0</div>
+          <div data-testid="header-currency-field">BRL</div>
+        </header>
+      </div>
+    );
   }
 }
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
